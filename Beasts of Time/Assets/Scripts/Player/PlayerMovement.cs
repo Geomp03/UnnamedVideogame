4853cf97dd604e3264d618069417b1f49178c89d
Georgios Pavlis <geomp03@gmail.com>
2023-04-20 23:05:21 +0100

    PlayerMovement

---
+++ Beasts of Time/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D playerRB;
+
+    [SerializeField] private float walkingSpeed = 5f;
+    [SerializeField] private float jumpForce = 500f;
+
+    private float DirX, DirY;
+    private bool jumpInput;
+    private int groundMask;
+    private bool isGrounded;
+
+    private void Awake()
+    {
+#if UNITY_EDITOR
+        Debug.unityLogger.logEnabled = true;
+#else
+        Debug.unityLogget.logEnabled = false;
+#endif
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+        groundMask = LayerMask.NameToLayer("Ground");
+        Debug.Log(groundMask);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check for horizontal movement inputs
+        DirX = Input.GetAxisRaw("Horizontal");
+
+        // Check if jump key is pressed - spacebar
+        if (Input.GetButtonDown("Jump"))
+            jumpInput = true;
+    }
+
+    // Physics based time step
+    private void FixedUpdate()
+    {
+        // Check if the player is grounded
+        RaycastHit2D hit2D = Physics2D.Raycast(transform.position, Vector2.down, 0.1f, groundMask);
+
+        if (hit2D.collider != null)
+        {
+            isGrounded = true;
+            Debug.Log("Player is grounded");
+        }
+        else
+        {
+            isGrounded = false;
+            Debug.Log("Player is not grounded");
+        }
+
+
+        // Update player movement from inputs
+        playerRB.velocity = new Vector2(DirX * walkingSpeed, playerRB.velocity.y);
+
+        // Check for jump
+        if(jumpInput && isGrounded)
+        {
+            playerRB.AddForce(new Vector2(0, jumpForce));
+            jumpInput = false;
+        }
+    }
+}
